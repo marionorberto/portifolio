31f300d19dbdcf2234a395a7ffa3055e9ae9450b
Mário Norberto <marionorberto2018@gmail.com>
2024-07-19 03:31:10 +0100

    initial commit

---
+++ src/app/components/matrix-background/matrix-background.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix-background',
+  standalone: true,
+  imports: [],
+  templateUrl: './matrix-background.component.html',
+  styleUrl: './matrix-background.component.css'
+})
+export class MatrixBackgroundComponent {
+
+}
